1b92f9cc9350c6aa2aa4f7448f17d2145c9909e9
i1i1 <vanyarybin1@live.ru>
2023-02-02 18:47:37 +0300

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -11,35 +11,37 @@
 
 
   outputs = { nixpkgs, home-manager, ... }:
-  let
-    system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-      config = { allowUnfree = true; };
-    };
-
-    lib = nixpkgs.lib;
-
-  in {
-    nixosConfigurations = {
-      i1i1 = lib.nixosSystem {
+      pkgs = import nixpkgs {
         inherit system;
+        config = { allowUnfree = true; };
+      };
 
-        modules = [
-          ./system/configuration.nix
-          # home-manager.nixosModules.home-manager {
-          #   home-manager = {
-          #     useGlobalPkgs = true;
-          #   };
-          # }
-        ];
+      lib = nixpkgs.lib;
+
+    in
+    {
+      nixosConfigurations = {
+        i1i1 = lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            ./system/configuration.nix
+            # home-manager.nixosModules.home-manager
+            # {
+            #   home-manager = {
+            #     useGlobalPkgs = true;
+            #   };
+            # }
+          ];
+        };
+      };
+      homeConfigurations.i1i1 = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        imports = [ ./user/i1i1/home.nix ];
       };
     };
-    homeConfigurations.i1i1 = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      imports = [ ./user/i1i1/home.nix ];
-    };
-  };
 }
 
