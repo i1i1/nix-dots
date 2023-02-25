46a2a2d46fd208b95ae008aab9dfe904b59896ed
i1i1 <vanyarybin1@live.ru>
2023-02-25 22:52:46 +0300

    Add metamask and some misk firefox settings

--- user/i1i1/firefox.nix
+++ user/i1i1/firefox.nix
@@ -6,27 +6,28 @@ let
       nixExtensions = [
         (fetchFirefoxAddon {
           name = "ublock";
-          url =
-            "https://addons.mozilla.org/firefox/downloads/file/3679754/ublock_origin-1.31.0-an+fx.xpi";
+          url = "https://addons.mozilla.org/firefox/downloads/file/3679754/ublock_origin-1.31.0-an+fx.xpi";
           sha256 = "1h768ljlh3pi23l27qp961v1hd0nbj2vasgy11bmcrlqp40zgvnr";
         })
         (fetchFirefoxAddon {
           name = "tree-style-tab";
-          url =
-            "https://addons.mozilla.org/firefox/downloads/file/4053198/tree_style_tab-3.9.12.xpi";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4053198/tree_style_tab-3.9.12.xpi";
           sha256 = "sha256-ii47IjdWwLqQlg7N3GTSgrTcgpOhwYyaGwx3kp3Kpbg=";
         })
         (fetchFirefoxAddon {
           name = "languagetool";
-          url =
-            "https://addons.mozilla.org/firefox/downloads/file/4034972/languagetool-6.0.1.xpi";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4034972/languagetool-6.0.1.xpi";
           sha256 = "sha256-pyQ5JY1+k3Ylt2KrTjXifYC927G/nFeUfmXslNhYfnk=";
         })
         (fetchFirefoxAddon {
           name = "bitwarden";
-          url =
-            "https://addons.mozilla.org/firefox/downloads/file/4054938/bitwarden_password_manager-2023.1.0.xpi";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4054938/bitwarden_password_manager-2023.1.0.xpi";
           sha256 = "sha256-sQeTD90AWqxpRiIquLgMeJ8XvY5szWnE9KHP4QKxyWQ=";
+        })
+        (fetchFirefoxAddon {
+          name = "metamask";
+          url = "https://addons.mozilla.org/firefox/downloads/file/4037096/ether_metamask-10.22.2.xpi";
+          sha256 = "sha256-G+MwJDOcsaxYSUXjahHJmkWnjLeQ0Wven8DU/lGeMzA=";
         })
       ];
 
@@ -38,6 +39,10 @@ let
         DisableFirefoxAccounts = true;
         FirefoxHome = {
           Pocket = false;
+          SponsoredPocket = false;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Search = false;
           Snippets = false;
         };
         SearchEngines = {
@@ -45,6 +50,10 @@ let
           PreventInstalls = true;
         };
         UserMessaging = {
+          WhatsNew = false;
+          FeatureRecommentdations = false;
+          UrlbarInterventions = false;
+          MoreFromMozilla = false;
           ExtensionRecommendations = false;
           SkipOnboarding = true;
         };
