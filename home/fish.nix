96b9bf311cabf0188a049e2cdf72aa12e03ee908
i1i1 <vanyarybin1@live.ru>
2023-02-26 13:48:57 +0300

    Format fish.nix

--- home/fish.nix
+++ home/fish.nix
@@ -17,28 +17,30 @@
       update-system = "nix flake update ~/.dotfiles";
     };
 
-    plugins = let
-      fromNix = name: {
-        name = name;
-        src = pkgs.fishPlugins.${name}.src;
-      };
-    in map fromNix [
-      "bass"
-      "colored-man-pages"
-      "done"
-      "fzf-fish"
-      "hydro"
-      "pisces"
-      "sponge"
-    ] ++ [{
-      name = "z";
-      src = pkgs.fetchFromGitHub {
-        owner = "jethrokuan";
-        repo = "z";
-        rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
-        sha256 = "1kaa0k9d535jnvy8vnyxd869jgs0ky6yg55ac1mxcxm8n0rh2mgq";
-      };
-    }];
+    plugins =
+      let
+        fromNix = name: {
+          name = name;
+          src = pkgs.fishPlugins.${name}.src;
+        };
+      in
+      map fromNix [
+        "bass"
+        "colored-man-pages"
+        "done"
+        "fzf-fish"
+        "hydro"
+        "pisces"
+        "sponge"
+      ] ++ [{
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "85f863f20f24faf675827fb00f3a4e15c7838d76";
+          sha256 = "1kaa0k9d535jnvy8vnyxd869jgs0ky6yg55ac1mxcxm8n0rh2mgq";
+        };
+      }];
 
     shellInit = ''
       function chhm
