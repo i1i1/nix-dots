5ee87cccc10c9f339ac6c2c7eb73b69e81127089
i1i1 <vanyarybin1@live.ru>
2023-03-15 19:40:07 +0300

    Persist docker directory

--- hardware/pc.nix
+++ hardware/pc.nix
@@ -21,6 +21,7 @@
       "/var/log"
       "/var/lib/bluetooth"
       "/var/lib/nixos"
+      "/var/lib/docker"
       "/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
     ];
