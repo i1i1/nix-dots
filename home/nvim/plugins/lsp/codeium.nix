1e2fd350f48096e404cbfabe97a1c0cb372a187d
i1i1 <vanyarybin1@live.ru>
2023-03-31 18:49:52 +0300

    Update codeium

--- home/nvim/plugins/lsp/codeium.nix
+++ home/nvim/plugins/lsp/codeium.nix
@@ -6,18 +6,18 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "jcdickinson";
       repo = "codeium.nvim";
-      rev = "de7c4d17f5b10e6c38b3c68d3472fffca984acae";
-      hash = "sha256-Wnc3k1gHVC/QQp6aqasYW7ABc+Knr2IQ8U6Lm35c0vE=";
+      rev = "55fa67bd316e2a4d312b11d68a2c34f898925a7f";
+      hash = "sha256-bmnEaNLxAsGPfiHwmwE/IZvhuBrOcSPFUnP5XCwPutg=";
     };
   };
 
   codeium = pkgs.stdenv.mkDerivation rec {
     pname = "codeium";
-    version = "1.1.42";
+    version = "1.1.69";
 
     src = pkgs.fetchurl {
       url = "https://github.com/Exafunction/codeium/releases/download/language-server-v${version}/language_server_linux_x64.gz";
-      sha256 = "sha256-sT0XoeXXMAJzZ/fMuWa4ukBceqInhn107cgUogdZnfg=";
+      sha256 = "sha256-RK2yjxAC88HT6GuXQRmjiDTbkEBU1vJ3a5N1g48pusM=";
     };
 
     unpackPhase = ''
